0a40c2822714348bbf7f808cf57c4f78536e7aa9
Olivier Philippon <olivier@textualize.io>
2022-04-12 12:12:28 +0100

    Replace our shiny projects TOML files with a JSON one
    It turns out that we cannot read files after build time when usign Next.js on Vercel: https://vercel.com/support/articles/how-can-i-use-files-in-serverless-functions#next.js
    
    To solve this we can import the data as a JSON file, which is then inlined in the build by the TypeScript compiler.
    
    - Downsides: JSON < TOML (imho 🤷)
    - Upsides: less code, less dependencies (we don't need a TOML parser any more), static check by TypeScript of what we do with the data in the JSON file

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,23 +1,9 @@
-import * as TOML from "@iarna/toml"
-import { readFile } from "fs/promises"
-import { join } from "path"
+import projectsData from "../../data/projects.json"
 import type { ProjectData } from "../domain"
 import * as githubServices from "./github"
 
-interface ProjectDataFromToml {
-    headline: string
-    stars: string
-    desc: string
-    code: string
-    docs?: string
-}
-
-const ProjectsDataFilePath = join(new URL(import.meta.url).pathname, "..", "..", "..", "data", "projects.toml")
-
 export async function projects(): Promise<ProjectData[]> {
-    const projects = await projectsRawData()
-
-    return projects.map((rawData) => {
+    return projectsData["projects"].map((rawData) => {
         return {
             headline: rawData.headline,
             stars: rawData.stars,
@@ -27,15 +13,4 @@ export async function projects(): Promise<ProjectData[]> {
             docsUrl: rawData.docs ?? null,
         }
     })
-}
-
-export async function projectsDataFileContent(): Promise<string> {
-    return await readFile(ProjectsDataFilePath.toString(), { encoding: "utf-8" })
-}
-
-export async function projectsRawData(): Promise<ProjectDataFromToml[]> {
-    const tomlContent = await projectsDataFileContent()
-    const tomlData = TOML.parse(tomlContent)
-
-    return (tomlData as any)["projects"] as ProjectDataFromToml[]
 }
