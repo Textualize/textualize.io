c3e0bb195709231a53c07dfe96a8a38c7e88d68b
Will McGugan <willmcgugan@gmail.com>
2022-04-14 09:12:07 +0100

    copy tweaks

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,19 +6,19 @@ export const Hero = () => {
     return (
         <section className="container hero">
             <div className="hero__text">
-                <h1 className="hero__headline">
-                    We love{" "}
+                <h1 className="hero__headline">                  
                     <span className="u-no-wrap">
-                        terminals
+                        We ♥ terminals
                         <span className="hero__cursor" />
                     </span>
                 </h1>
                 <p className="hero__desc">
-                    At Textualize, we are working on making terminals more <strong>beautiful</strong> and{" "}
-                    <strong>powerful</strong> than ever before.{" "}
+                   The terminal can be more <strong>powerful</strong> and <strong>beautiful</strong> than you ever thought.
+                   <br/><br/>
+                    <a href="#">Read more...</a>
                 </p>
                 <a href={AppConfig.hiringUrl} className="button button--hero" target="_blank" rel="noreferrer">
-                    &gt; We&apos;re&nbsp;<b> hiring</b>!_
+                    We&apos;re&nbsp;<b> hiring!</b>
                 </a>
             </div>
             <div className="hero__terminal-wrapper">
