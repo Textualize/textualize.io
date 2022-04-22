bef75153316b3715c072b5d2098a69293fabe852
Olivier Philippon <olivier@textualize.io>
2022-04-22 10:57:03 +0100

    [SEO] Add "social media" metatags

---
+++ src/components/social-media-meta-tags.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+// Shared content
+const TITLE = `Textualize.io`
+const DESCRIPTION = `The terminal can be more powerful and beautiful than you ever thought`
+const IMAGE_URL = "https://res.cloudinary.com/textualize-io/image/upload/v1650621291/social%20media%20share%20image.png"
+const SITE_URL = "https://www.textualize.io/"
+// Twitter-specific stuff
+const TWITTER_SITE = "@textualizeio"
+// OpenGraph-specific stuff
+const OPENGRAPH_TYPE = "website"
+
+export function SocialMediaMetaTags(): JSX.Element {
+    // Basically modeled after `view-source:https://github.com/Textualize/rich` ^_^
+
+    return (
+        <>
+            {/* Let's start with Twitter-specific tags...*/}
+            {/* @link https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
+            <meta name="twitter:image:src" content={IMAGE_URL} />
+            <meta name="twitter:site" content={TWITTER_SITE} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={TITLE} />
+            <meta name="twitter:description" content={DESCRIPTION} />
+            {/* And now for the OpenGraph tags...*/}
+            {/* @link https://ogp.me/ */}
+            <meta property="og:image" content={IMAGE_URL} />
+            <meta property="og:image:alt" content={DESCRIPTION} />
+            <meta property="og:type" content={OPENGRAPH_TYPE} />
+            <meta property="og:title" content={TITLE} />
+            <meta property="og:url" content={SITE_URL} />
+            <meta property="og:description" content={DESCRIPTION} />
+        </>
+    )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,17 @@
+import React from "react"
 import { Head, Html, Main, NextScript } from "next/document"
+import { SocialMediaMetaTags } from "../components/social-media-meta-tags"
 
 export default function Document() {
-    // Just to add `lang="en"`...
     // @link https://nextjs.org/docs/advanced-features/custom-document
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link rel="icon" href="/textualize-logo.svg" />
+                {/* @link https://nextjs.org/docs/basic-features/font-optimization */}
+                <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&amp;display=optional" rel="stylesheet" />
+                <SocialMediaMetaTags />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
